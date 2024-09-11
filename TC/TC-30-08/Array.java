94dff10fcff8e4e14dd1601d7f59f413ef53f886
Shir <123198641+cascanteshir@users.noreply.github.com>
2024-09-10 20:53:20 -0600

    Crear Array.java

---
+++ TC/TC-30-08/Array.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Array {
+
+    private int[] arreglo = new int[5];
+
+    public void solicitarNumero(){
+        
+        Scanner escaner = new Scanner(System.in);
+        
+        for(int i = 0; i < 5; i++){
+            System.out.print("Ingrese un número: ");
+            arreglo[i] = escaner.nextInt();
+        }
+        escaner.close();
+    }
+    
+    public void sumaTotal(){
+        int suma = 0;
+        
+        for(int i = 0; i < arreglo.length; i++){
+            suma += arreglo[i];
+        }
+        System.out.println(suma);
+    }
+    
+    public static void main(String[] args){  
+        Array array = new Array();  
+        array.solicitarNumero(); 
+        array.sumaTotal(); 
+    }
+}
