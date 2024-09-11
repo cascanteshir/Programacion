580b0ccf1ebc32b475d9227e0af9a3db60d66714
Shir <123198641+cascanteshir@users.noreply.github.com>
2024-09-10 20:54:57 -0600

    Crear Arreglo.java

---
+++ TC/TC-03-09/arreglo_de_10/Arreglo.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Arreglo
+{
+   
+    public int[] crearArreglo(){
+        int[] arreglo = new int[10];
+        
+        for(int i = 0; i < 10; i++){
+            arreglo[i] = (int) (Math.random() * 100 + 1);
+        }
+        
+        return arreglo;
+    }
+
+    public int[] sumarNum (int[] arreglo, int numero)
+    {
+        int[] arregloSumado = new int[10];
+        for(int i = 0; i < 10; i++){
+            arregloSumado[i] = arreglo[i] + numero;
+        }
+        return arregloSumado;
+    }
+    
+    public static void main(String[] args){
+        Scanner escaner = new Scanner(System.in);
+        
+        Arreglo objetoArreglo = new Arreglo();
+        
+        int[] arreglo = objetoArreglo.crearArreglo();
+        for(int i = 0; i < 10; i++){
+            System.out.print(arreglo[i] + ", ");
+        }
+        
+        System.out.println("\nIngrese un numero");
+        int numero = escaner.nextInt();
+        
+        int[] resultados = objetoArreglo.sumarNum(arreglo, numero);
+        
+        for(int i = 0; i < 10; i++){
+            System.out.print(resultados[i] + ", ");
+        }
+    }
+}
