1d4a08d90b013b40f71dd2a21d9bd196b74eea36
NathalieAlfaro <nathalie.alfaro@ucr.ac.cr>
2024-11-10 17:00:45 -0600

    NOTA

--- TC/TC-27-08/Mensajero.java
+++ TC/TC-27-08/Mensajero.java
@@ -1,10 +1,20 @@
 public class Mensajero {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Codificador codificador = new Codificador();
-
         codificador.solicitarFraseYNum();
-
         String palabra = codificador.codificar(codificador.frase, codificador.numero);
         System.out.println(palabra);
     }
 }
+
+
+/*
+-5 porque su salida da primero un null
+Entonces 95
+
+Ingrese una frase
+olla
+Ingrese un número
+66
+nullo66l66l66a66
+ */
