c9a6b9b448f972cd4c6af3b12e4620dd10a3b523
NathalieAlfaro <nathalie.alfaro@ucr.ac.cr>
2024-11-10 17:00:32 -0600

    Le di formato

--- TC/TC-27-08/Codificador.java
+++ TC/TC-27-08/Codificador.java
@@ -5,19 +5,17 @@ public class Codificador {
     int numero;
     String codigo;
 
-    public void solicitarFraseYNum(){
+    public void solicitarFraseYNum() {
         Scanner escaner = new Scanner(System.in);
-
         System.out.println("Ingrese una frase");
         frase = escaner.nextLine();
         System.out.println("Ingrese un número");
         numero = escaner.nextInt();
         escaner.close();
-
     }
 
-    public String codificar(String frase, int numero){
-        for(int i = 0; i < frase.length(); i++){
+    public String codificar(String frase, int numero) {
+        for (int i = 0; i < frase.length(); i++) {
             codigo += frase.charAt(i);
             codigo += numero;
         }
