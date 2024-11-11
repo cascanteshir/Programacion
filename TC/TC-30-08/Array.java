3dff3d0c4dbea8e704c10fa3a0c88414636fbe06
NathalieAlfaro <nathalie.alfaro@ucr.ac.cr>
2024-11-10 21:58:40 -0600

    NOTA

--- TC/TC-30-08/Array.java
+++ TC/TC-30-08/Array.java
@@ -1,32 +1,31 @@
+// 100
+
 import java.util.Scanner;
 
 public class Array {
 
     private int[] arreglo = new int[5];
 
-    public void solicitarNumero(){
-        
+    public void solicitarNumero() {
         Scanner escaner = new Scanner(System.in);
-        
-        for(int i = 0; i < 5; i++){
+        for (int i = 0; i < 5; i++) {
             System.out.print("Ingrese un número: ");
             arreglo[i] = escaner.nextInt();
         }
         escaner.close();
     }
-    
-    public void sumaTotal(){
+
+    public void sumaTotal() {
         int suma = 0;
-        
-        for(int i = 0; i < arreglo.length; i++){
+        for (int i = 0; i < arreglo.length; i++) {
             suma += arreglo[i];
         }
         System.out.println(suma);
     }
-    
-    public static void main(String[] args){  
-        Array array = new Array();  
-        array.solicitarNumero(); 
-        array.sumaTotal(); 
+
+    public static void main(String[] args) {
+        Array array = new Array();
+        array.solicitarNumero();
+        array.sumaTotal();
     }
 }
